2d4ec4496ec5a7e60f28dc1a356e454114560263
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-05-31 23:50:46 +0530

    created models

---
+++ models/bookmark.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const bookmarkSchema = new mongoose.Schema({
+   name: {
+      type:String,
+      required:true
+   },
+   url:{
+      type:String,
+      required:true,
+   },
+   category: {
+      type:String,
+      enum:['sports','entertainment','news','cooking', 'crafts'],
+      required:true,
+      default:'entertainment',
+   },
+   user:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required:true,
+      index:true,
+   },
+
+},{timestamps:true});
+
+export const Bookmark = mongoose.model('Bookmark',bookmarkSchema);
+
+// name, url, category, user, 
