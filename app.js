95e70e54faf9f5699661280329af4f4776c5691c
Mahesh Kumar G <maheshh.kumar1508@gmail.com>
2025-06-01 12:48:53 +0530

    error handling + error middlewares

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ import connectToDB from './database/mongodb.js';
 import authRouter from './routes/auth.route.js';
 import bookmarkRouter from './routes/bookmark.route.js';
 import userRouter from './routes/user.route.js';
+import errorMiddleware from './middleware/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 5500;
 
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/bookmark', bookmarkRouter);
 app.use('/api/v1/user', userRouter);
 
+// error handling
+app.use(errorMiddleware);
 
 app.get('/',(req, res)=>{
    res.send('Welcome to bookmark vault system');
---
+++ middleware/error.middleware.js
@@ -0,0 +1,18 @@
+const errorMiddleware = (err, req, res,next)=>{
+   try {
+
+      let error = {...err};
+      error.message = err.message;
+      console.log(err.message);
+
+      res.json(error.statusCode||500).json({
+         sucess:false,
+         errorMessage:error.message
+      });
+
+   } catch (error) {
+      next(error);
+   }
+}
+
+export default errorMiddleware;
